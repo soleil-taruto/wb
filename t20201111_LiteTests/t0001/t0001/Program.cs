eac7f7f85b2677bf666105e661262328e1bb953c
soleil-taruto <soleil.taruto@gmail.com>
2020-11-12 00:48:39 +0900

    Backup 1111

---
+++ t20201111_LiteTests/t0001/t0001/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				new Program().Main2();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+
+			Console.WriteLine("OK!");
+			Console.ReadLine();
+		}
+
+		private void Main2()
+		{
+			// -- choose one --
+
+			//new Test0001().Test01();
+			new Test0001().Test02();
+			//new Test0001().Test03();
+
+			// --
+		}
+	}
+}
---
+++ t20201111_LiteTests/t0001/t0001/Tests/Test0001.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.Tests
+{
+	public class Test0001
+	{
+		public void Test01()
+		{
+			Console.WriteLine(123456789ul.ToString("D12"));
+		}
+
+		public void Test02()
+		{
+			bool f = false;
+
+			Console.WriteLine(f);
+			f ^= 1 == 2; // false ^ false -> false
+			Console.WriteLine(f);
+			f ^= 1 == 1; // false ^ true -> true
+			Console.WriteLine(f);
+			f ^= 1 == 2; // true ^ false -> true
+			Console.WriteLine(f);
+			f ^= 1 == 1; // true ^ true -> false
+			Console.WriteLine(f);
+		}
+	}
+}
