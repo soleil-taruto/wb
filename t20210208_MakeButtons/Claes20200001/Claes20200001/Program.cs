f8f774069c00cd8f1b6e540cd3ee51f70bfe6138
soleil-taruto <soleil.taruto@gmail.com>
2021-02-09 12:52:39 +0900

    Backup 20210209125230

---
+++ t20210208_MakeButtons/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			SCommon.DeletePath(Consts.OUTPUT_DIR);
+			SCommon.CreateDir(Consts.OUTPUT_DIR);
+
+			// -- choose one --
+
+			MakeButtons_20210208();
+			//new Test0001().Test01();
+			//new Test0001().Test02();
+			//new Test0002().Test01();
+
+			// --
+
+			//Console.WriteLine("Press ENTER key.");
+			//Console.ReadLine();
+		}
+
+		private void MakeButtons_20210208()
+		{
+			//MakeButtons_20210208_a("フルスクリーン", 50);
+			//MakeButtons_20210208_a("ウィンドウ", 300);
+
+			MakeButtons_20210208_a("はじめから", 300);
+			MakeButtons_20210208_a("つづきから", 300);
+			MakeButtons_20210208_a("コンフィグ", 300);
+			MakeButtons_20210208_a("ＣＧモード", 300);
+			MakeButtons_20210208_a("回想モード", 300);
+			MakeButtons_20210208_a("ＥＮＤ", 550);
+		}
+
+		private void MakeButtons_20210208_a(string text, int text_x)
+		{
+			int w = 2400;
+			int h = 480;
+			int frame = 60;
+
+			I4Color backColor = new I4Color(255, 255, 255, 0);
+			I4Color frameColor = new I4Color(255, 128, 192, 255);
+			I4Color textColor = new I4Color(255, 255, 255, 255);
+
+			Canvas canvas = new Canvas(w, h);
+
+			canvas.Fill(backColor);
+			canvas.DrawCircle(new D2Point(0 + h / 2, h / 2), h / 2, frameColor);
+			canvas.DrawCircle(new D2Point(w - h / 2, h / 2), h / 2, frameColor);
+			canvas.FillRect(new I4Rect(h / 2, frame * 0, w - h, h - frame * 0), frameColor);
+			canvas.FillRect(new I4Rect(h / 2, frame * 1, w - h, h - frame * 2), backColor);
+
+			canvas = canvas.DrawString(text, 180, textColor, h / 2 + text_x, 70);
+
+			canvas = canvas.Expand(w / 6, h / 6);
+
+			canvas.Save(Path.Combine(Consts.OUTPUT_DIR, Common.ZenToHan(text) + ".png"));
+		}
+	}
+}
---
+++ t20210208_MakeButtons/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Charlotte.Commons;
+
+namespace Charlotte.Tests
+{
+	public class Test0001
+	{
+		public void Test01()
+		{
+			{
+				Canvas canvas = Canvas.Load(@"C:\etc\画像\1875384.jpg");
+				canvas = canvas.Expand(777, 777);
+				canvas.Save(@"C:\temp\1.png");
+			}
+
+			{
+				Canvas canvas = Canvas.Load(@"C:\etc\画像\80347715_p7.png");
+				canvas = canvas.Expand(777, 1111);
+				canvas.Save(@"C:\temp\2.png");
+			}
+		}
+
+		public void Test02()
+		{
+			{
+				Canvas canvas = new Canvas(1200, 800);
+
+				canvas.FillRect(new I4Rect(0, 0, canvas.W, canvas.H), new I4Color(255, 255, 255, 0));
+
+				canvas.DrawCircle(new D2Point(100, 100), 100, new I4Color(0, 0, 255, 255));
+				canvas.DrawCircle(new D2Point(1100, 100), 100, new I4Color(0, 0, 255, 255));
+				canvas.DrawCircle(new D2Point(1100, 700), 100, new I4Color(0, 0, 255, 255));
+				canvas.DrawCircle(new D2Point(100, 700), 100, new I4Color(0, 0, 255, 255));
+				canvas.FillRect(new I4Rect(100, 0, 1000, 800), new I4Color(0, 0, 255, 255));
+				canvas.FillRect(new I4Rect(0, 100, 1200, 600), new I4Color(0, 0, 255, 255));
+
+				canvas.DrawCircle(new D2Point(100, 100), 50, new I4Color(255, 255, 0, 255));
+				canvas.DrawCircle(new D2Point(1100, 100), 50, new I4Color(255, 255, 0, 255));
+				canvas.DrawCircle(new D2Point(1100, 700), 50, new I4Color(255, 255, 0, 255));
+				canvas.DrawCircle(new D2Point(100, 700), 50, new I4Color(255, 255, 0, 255));
+				canvas.FillRect(new I4Rect(100, 50, 1000, 700), new I4Color(255, 255, 0, 255));
+				canvas.FillRect(new I4Rect(50, 100, 1100, 600), new I4Color(255, 255, 0, 255));
+
+				canvas = canvas.DrawString("テ1", 400, new I4Color(255, 0, 0, 255), 50, 50);
+
+				canvas = canvas.Expand(300, 200);
+
+				canvas.Save(@"C:\temp\1.png");
+			}
+
+			{
+				Canvas canvas = new Canvas(1200, 800);
+
+				canvas.FillRect(new I4Rect(0, 0, canvas.W, canvas.H), new I4Color(255, 255, 255, 0));
+
+				canvas.DrawCircle(new D2Point(200, 200), 200, new I4Color(0, 0, 255, 255));
+				canvas.DrawCircle(new D2Point(1000, 200), 200, new I4Color(0, 0, 255, 255));
+				canvas.DrawCircle(new D2Point(1000, 600), 200, new I4Color(0, 0, 255, 255));
+				canvas.DrawCircle(new D2Point(200, 600), 200, new I4Color(0, 0, 255, 255));
+				canvas.FillRect(new I4Rect(200, 0, 800, 800), new I4Color(0, 0, 255, 255));
+				canvas.FillRect(new I4Rect(0, 200, 1200, 400), new I4Color(0, 0, 255, 255));
+
+				canvas.DrawCircle(new D2Point(200, 200), 100, new I4Color(255, 255, 0, 255));
+				canvas.DrawCircle(new D2Point(1000, 200), 100, new I4Color(255, 255, 0, 255));
+				canvas.DrawCircle(new D2Point(1000, 600), 100, new I4Color(255, 255, 0, 255));
+				canvas.DrawCircle(new D2Point(200, 600), 100, new I4Color(255, 255, 0, 255));
+				canvas.FillRect(new I4Rect(200, 100, 800, 600), new I4Color(255, 255, 0, 255));
+				canvas.FillRect(new I4Rect(100, 200, 1000, 400), new I4Color(255, 255, 0, 255));
+
+				canvas = canvas.DrawString("テ2", 400, new I4Color(255, 0, 0, 255), 50, 50);
+
+				canvas = canvas.Expand(300, 200);
+
+				canvas.Save(@"C:\temp\2.png");
+			}
+		}
+	}
+}
