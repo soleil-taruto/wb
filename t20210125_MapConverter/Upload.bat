4ca97ecf64f644c50401d665ebe0afa4a3299ef7
soleil-taruto <soleil.taruto@gmail.com>
2021-02-26 19:40:03 +0900

    Backup 20210226193954

--- t20210125_MapConverter/Upload.bat
+++ t20210125_MapConverter/Upload.bat
@@ -4,4 +4,4 @@ cx **
 CALL Release.bat /B
 
 C:\Factory\Petra\UpdatingCopy.exe out C:\be\Web\DocRoot\wb\d20210125_MapConverter
-C:\Factory\Petra\RunOnBatch.exe C:\be\Web Deploy.bat
+C:\Factory\Petra\RunOnBatch.exe C:\be\Web Upload.bat
