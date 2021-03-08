491bed1ba3737545e5fc9e23bd3ead5071ac381b
soleil-taruto <soleil.taruto@gmail.com>
2021-03-08 21:37:02 +0900

    Backup 20210308213659

---
+++ t20210241_Opacify/Claes20200001/Claes20200001/Margin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte
+{
+	public class Margin
+	{
+		public int L;
+		public int T;
+		public int R;
+		public int B;
+
+		public Margin(int margin)
+		{
+			this.L = margin;
+			this.T = margin;
+			this.R = margin;
+			this.B = margin;
+		}
+
+		public Margin(int l, int t, int r, int b)
+		{
+			this.L = l;
+			this.T = t;
+			this.R = r;
+			this.B = b;
+		}
+	}
+}
---
+++ t20210241_Opacify/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			SCommon.DeletePath(Consts.OUTPUT_DIR);
+			SCommon.CreateDir(Consts.OUTPUT_DIR);
+
+			// -- choose one --
+
+			Main3();
+			//new Test0001().Test01();
+			//new Test0001().Test02();
+			//new Test0002().Test01();
+
+			// --
+
+			//Console.WriteLine("Press ENTER key.");
+			//Console.ReadLine();
+		}
+
+		private void Main3()
+		{
+			Main3_a(@"C:\wb\20210214_シャニマス画像DL");
+		}
+
+		private void Main3_a(string dir)
+		{
+			foreach (string file in Directory.GetFiles(dir).Sort(SCommon.CompIgnoreCase))
+			{
+				Canvas canvas = Canvas.Load(file);
+
+				{
+					Canvas dest = new Canvas(canvas.W, canvas.H);
+
+					dest.Fill(new I4Color(255, 255, 255, 255));
+					dest.DrawImage(canvas, 0, 0, true);
+
+					canvas = dest;
+				}
+
+				canvas.Save(Path.Combine(Consts.OUTPUT_DIR, Path.GetFileName(file)));
+			}
+		}
+	}
+}
