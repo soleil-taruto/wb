91878813a24d4cf30b84e26fbfa047689240b871
soleil-taruto <soleil.taruto@gmail.com>
2021-03-13 00:19:56 +0900

    Backup 20210313001955

---
+++ t20210314/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			// -- choose one --
+
+			//Main3();
+			Main3_0002();
+			//new Test0001().Test01();
+			//new Test0001().Test02();
+			//new Test0002().Test01();
+
+			// --
+
+			//Console.WriteLine("Press ENTER key.");
+			//Console.ReadLine();
+
+			Common.OpenOutputDirIfCreated();
+		}
+
+		private void Main3()
+		{
+			File.WriteAllText(Common.NextOutputPath() + ".txt", "1");
+			File.WriteAllText(Common.NextOutputPath() + ".txt", "2");
+			File.WriteAllText(Common.NextOutputPath() + ".txt", "3");
+		}
+
+		private void Main3_0002()
+		{
+			// noop
+		}
+	}
+}
