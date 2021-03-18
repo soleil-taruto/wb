f4b0e7e2ce373e8556a75c81d8336c477d2747aa
soleil-taruto <soleil.taruto@gmail.com>
2021-03-18 16:34:29 +0900

    Voyager 1 is 22777096636.066 kilometers away from the Sun.

---
+++ t20210318_VoyagerDistance/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			SCommon.DeletePath(Consts.OUTPUT_DIR);
+			SCommon.CreateDir(Consts.OUTPUT_DIR);
+
+			// -- choose one --
+
+			Main3();
+			//new Test0001().Test01();
+			//new Test0001().Test02();
+			//new Test0002().Test01();
+
+			// --
+
+			Console.WriteLine("Press ENTER key.");
+			Console.ReadLine();
+		}
+
+		private void Main3()
+		{
+			VoyagerDistance vd = new VoyagerDistance();
+
+			Print(vd.Earth_Voyager_1, "Earth to Voyager 1");
+			Print(vd.Earth_Voyager_2, "Earth to Voyager 2");
+			Print(vd.Sun_Voyager_1, "Sun to Voyager 1");
+			Print(vd.Sun_Voyager_2, "Sun to Voyager 2");
+		}
+
+		private void Print(VoyagerDistance.DistancePairInfo pair, string prompt)
+		{
+			Print(pair.A, prompt);
+			Print(pair.B, prompt);
+
+			SCommon.SimpleDateTime now = SCommon.SimpleDateTime.Now();
+
+			Console.WriteLine(prompt + " --> " + now + ", " + pair.GetKm(now) + " KM (NOW)");
+		}
+
+		private void Print(VoyagerDistance.DistanceInfo info, string prompt)
+		{
+			Console.WriteLine(prompt + " --> " + info.DateTime + ", " + info.Km + " KM");
+		}
+	}
+}
---
+++ t20210318_VoyagerDistance/Claes20200001/Claes20200001/VoyagerDistance.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+using System.IO;
+
+// ^ sync @ VoyagerDistance
+
+namespace Charlotte
+{
+	// sync > @ VoyagerDistance
+
+	public class VoyagerDistance
+	{
+		private const string NASA_DISTANCE_DATA_URL = "https://voyager.jpl.nasa.gov/assets/javascripts/distance_data.js";
+
+		public class DistanceInfo
+		{
+			public SCommon.SimpleDateTime DateTime;
+			public double Km;
+		}
+
+		public class DistancePairInfo
+		{
+			public DistanceInfo A;
+			public DistanceInfo B;
+
+			public double GetKm(SCommon.SimpleDateTime dt)
+			{
+				double rateOfDateTime = Common.RateAToB(this.A.DateTime.ToSec(), this.B.DateTime.ToSec(), dt.ToSec());
+				double km = Common.AToBRate(this.A.Km, this.B.Km, rateOfDateTime);
+				return km;
+			}
+		}
+
+		public DistancePairInfo Earth_Voyager_1;
+		public DistancePairInfo Earth_Voyager_2;
+		public DistancePairInfo Sun_Voyager_1;
+		public DistancePairInfo Sun_Voyager_2;
+
+		public VoyagerDistance()
+		{
+			try
+			{
+				this.LoadNasaData();
+			}
+			catch
+			{
+				try
+				{
+					this.LoadNasaData(); // リトライ_1回目
+				}
+				catch
+				{
+					this.LoadNasaData(); // リトライ_2回目
+				}
+			}
+		}
+
+		private void LoadNasaData()
+		{
+			using (WorkingDir wd = new WorkingDir())
+			{
+				SCommon.Batch(new string[] { "curl -o out.txt " + NASA_DISTANCE_DATA_URL }, wd.GetPath("."));
+
+				string[] lines = File.ReadAllLines(wd.GetPath("out.txt"), Encoding.ASCII);
+				int c = 0;
+
+				long epoch_0 = long.Parse(ParseValue(lines[c++]));
+				long epoch_1 = long.Parse(ParseValue(lines[c++]));
+				c++;
+				double dist_0_v1 = double.Parse(ParseValue(lines[c++]));
+				double dist_1_v1 = double.Parse(ParseValue(lines[c++]));
+				c++;
+				double dist_0_v2 = double.Parse(ParseValue(lines[c++]));
+				double dist_1_v2 = double.Parse(ParseValue(lines[c++]));
+				c++;
+				double dist_0_v1s = double.Parse(ParseValue(lines[c++]));
+				double dist_1_v1s = double.Parse(ParseValue(lines[c++]));
+				c++;
+				double dist_0_v2s = double.Parse(ParseValue(lines[c++]));
+				double dist_1_v2s = double.Parse(ParseValue(lines[c++]));
+
+				if (c != lines.Length)
+					throw new Exception("FORMAT_ERROR");
+
+				long GMT_TO_JST = 9 * 3600;
+
+				SCommon.SimpleDateTime dateTime_0 = new SCommon.SimpleDateTime(epoch_0 + SCommon.TimeStampToSec.ToSec(19700101000000) + GMT_TO_JST);
+				SCommon.SimpleDateTime dateTime_1 = new SCommon.SimpleDateTime(epoch_1 + SCommon.TimeStampToSec.ToSec(19700101000000) + GMT_TO_JST);
+
+				this.Earth_Voyager_1 = new DistancePairInfo()
+				{
+					A = new DistanceInfo() { DateTime = dateTime_0, Km = dist_0_v1 },
+					B = new DistanceInfo() { DateTime = dateTime_1, Km = dist_1_v1 },
+				};
+				this.Earth_Voyager_2 = new DistancePairInfo()
+				{
+					A = new DistanceInfo() { DateTime = dateTime_0, Km = dist_0_v2 },
+					B = new DistanceInfo() { DateTime = dateTime_1, Km = dist_1_v2 },
+				};
+				this.Sun_Voyager_1 = new DistancePairInfo()
+				{
+					A = new DistanceInfo() { DateTime = dateTime_0, Km = dist_0_v1s },
+					B = new DistanceInfo() { DateTime = dateTime_1, Km = dist_1_v1s },
+				};
+				this.Sun_Voyager_2 = new DistancePairInfo()
+				{
+					A = new DistanceInfo() { DateTime = dateTime_0, Km = dist_0_v2s },
+					B = new DistanceInfo() { DateTime = dateTime_1, Km = dist_1_v2s },
+				};
+			}
+		}
+
+		private string ParseValue(string line)
+		{
+			int p = line.IndexOf('=');
+
+			if (p == -1)
+				throw new Exception("FORMAT_ERROR");
+
+			line = line.Substring(p + 1);
+			p = line.IndexOf(';');
+
+			if (p == -1)
+				throw new Exception("FORMAT_ERROR");
+
+			line = line.Substring(0, p);
+			line = line.Trim();
+			return line;
+		}
+	}
+
+	// < sync
+}
