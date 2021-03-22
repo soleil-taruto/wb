b07da7b371dd0ed10c2c1efb99bea46a417c1796
soleil-taruto <soleil.taruto@gmail.com>
2021-03-23 01:33:58 +0900

    Voyager 1 is 22783485214.895 kilometers away from the Sun.

---
+++ t20210325/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			if (ProcMain.DEBUG)
+			{
+				Main3();
+			}
+			else
+			{
+				Main4();
+			}
+		}
+
+		private void Main3()
+		{
+			// -- choose one --
+
+			Main4();
+			//new Test0001().Test01();
+			//new Test0001().Test02();
+			//new Test0002().Test01();
+
+			// --
+
+			//Common.Pause();
+
+			Common.OpenOutputDirIfCreated();
+		}
+
+		private void Main4()
+		{
+			Main4(ProcMain.ArgsReader);
+		}
+
+		private void Main4(ArgsReader ar)
+		{
+			// none
+		}
+	}
+}
