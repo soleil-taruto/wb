5d40402f4aaa4bbdfb9caedce7b5d0d24e7621eb
soleil-taruto <soleil.taruto@gmail.com>
2021-03-28 05:33:28 +0900

    Voyager 1 is 22791029702.042 kilometers away from the Sun.

--- t20210330_HTTAccessLogReport/Claes20200001/Claes20200001/Program.cs
+++ t20210330_HTTAccessLogReport/Claes20200001/Claes20200001/Program.cs
@@ -59,8 +59,7 @@ namespace Charlotte
 				logFiles = logFiles.Take(logFiles.Length - 1).ToArray(); // 最新のファイルは出力中かもしれないので除外する。
 
 			EraseKnownLogFiles(ref logFiles);
-			CollectReport(logFiles);
-			PrintReportLines();
+			AggregateAndReport(logFiles);
 		}
 
 		public string[] P_GetFiles(string dir)
@@ -89,14 +88,112 @@ namespace Charlotte
 				File.WriteAllText(Consts.LAST_LOG_FILE_SAVE_FILE, logFiles[logFiles.Length - 1], Encoding.UTF8); // 更新
 		}
 
-		private void CollectReport(string[] logFiles)
+		private void AggregateAndReport(string[] logFiles)
 		{
-			throw new NotImplementedException();
+			using (Aggregate aggrIP = new Aggregate())
+			using (Aggregate aggrRequest = new Aggregate())
+			using (Aggregate aggrHost = new Aggregate())
+			using (Aggregate aggrAgent = new Aggregate())
+			using (Aggregate aggrDomain = new Aggregate())
+			using (Aggregate aggrPath = new Aggregate())
+			using (Aggregate aggrStatus = new Aggregate())
+			{
+				foreach (string logFile in logFiles)
+				{
+					using (StreamReader reader = new StreamReader(logFile, SCommon.ENCODING_SJIS))
+					{
+						foreach (string line in IgnoreLocalIPRequest(ReadLogLines(reader)))
+						{
+							if (IsIPLine(line))
+							{
+								aggrIP.Add(line);
+							}
+							else if (line[0] == 'R')
+							{
+								aggrRequest.Add(line.Substring(1));
+							}
+							else if (line[0] == 'H')
+							{
+								aggrHost.Add(line.Substring(1));
+							}
+							else if (line[0] == 'A')
+							{
+								aggrAgent.Add(line.Substring(1));
+							}
+							else if (line[0] == 'D')
+							{
+								aggrDomain.Add(line.Substring(1));
+							}
+							else if (line[0] == 'P')
+							{
+								aggrPath.Add(line.Substring(1));
+							}
+							else if (line[0] == 'S')
+							{
+								aggrStatus.Add(line.Substring(1));
+							}
+						}
+					}
+				}
+				PrintReportLines(aggrIP, "IP");
+				PrintReportLines(aggrRequest, "Request");
+				PrintReportLines(aggrHost, "Host");
+				PrintReportLines(aggrAgent, "Agent");
+				PrintReportLines(aggrDomain, "Domain");
+				PrintReportLines(aggrPath, "Path");
+				PrintReportLines(aggrStatus, "Status");
+			}
 		}
 
-		private void PrintReportLines()
+		private IEnumerable<string> ReadLogLines(StreamReader reader)
 		{
-			throw new NotImplementedException();
+			for (; ; )
+			{
+				string line = reader.ReadLine();
+
+				if (line == null)
+					break;
+
+				line = SCommon.ToJString(SCommon.ENCODING_SJIS.GetBytes(line), true, false, false, true); // 2bs?
+
+				if (line == "")
+					continue;
+
+				yield return line;
+			}
+		}
+
+		private IEnumerable<string> IgnoreLocalIPRequest(IEnumerable<string> lines)
+		{
+			const string LOCAL_IP = "192.168.123.254"; // 室内(内側)ルータからのリクエストのIPアドレス
+
+			bool allowFlag = true;
+
+			foreach (string line in lines)
+			{
+				if (line == LOCAL_IP)
+					allowFlag = false;
+				else if (IsIPLine(line))
+					allowFlag = true;
+
+				if (allowFlag)
+					yield return line;
+			}
+		}
+
+		private void PrintReportLines(Aggregate aggr, string title)
+		{
+			Console.WriteLine("[" + title + "]");
+
+			foreach (string line in aggr.Top(30))
+			{
+				Console.WriteLine(line);
+			}
+		}
+
+		private static bool IsIPLine(string line)
+		{
+			return Regex.IsMatch(line, @"^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$");
 		}
 	}
 }
