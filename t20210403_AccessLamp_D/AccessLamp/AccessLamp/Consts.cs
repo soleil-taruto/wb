c8e8b905b61eb69897889bf09e44dad1eb13bf02
soleil-taruto <soleil.taruto@gmail.com>
2021-04-03 06:11:48 +0900

    Voyager 1 is 22799830655.859 kilometers away from the Sun.

---
+++ t20210403_AccessLamp_D/AccessLamp/AccessLamp/Consts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessLamp
+{
+	public static class Consts
+	{
+		public const int PICTURE_W = 60;
+		public const int PICTURE_H = 20;
+	}
+}
