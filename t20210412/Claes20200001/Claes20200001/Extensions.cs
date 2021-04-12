d158213f4f646096dbfc8fa12c796a2ac36700f6
soleil-taruto <soleil.taruto@gmail.com>
2021-04-12 19:27:10 +0900

    Voyager 1 is 22813780038.361 kilometers away from the Sun.

---
+++ t20210412/Claes20200001/Claes20200001/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte
+{
+	public static class Extensions
+	{
+		// sync > @ ForEach_Sort
+
+		public static IEnumerable<T> ForEach<T>(this IEnumerable<T> src, Action<T> action)
+		{
+			foreach (T element in src)
+				action(element);
+
+			return src;
+		}
+
+		public static IEnumerable<T> Sort<T>(this IEnumerable<T> src, Comparison<T> comp)
+		{
+#if true
+			{
+				List<T> list = src.ToList();
+				list.Sort(comp);
+				src = list;
+			}
+#else // src自体をソートしても良い場合
+			if (src is T[])
+			{
+				Array.Sort((T[])src, comp);
+			}
+			else if (src is List<T>)
+			{
+				((List<T>)src).Sort(comp);
+			}
+			else
+			{
+				List<T> list = src.ToList();
+				list.Sort(comp);
+				src = list;
+			}
+#endif
+			return src;
+		}
+
+		// < sync
+	}
+}
