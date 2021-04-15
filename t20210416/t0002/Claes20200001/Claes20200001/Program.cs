71b43992a69c8b6337ac781a431df40181b8478d
soleil-taruto <soleil.taruto@gmail.com>
2021-04-16 06:36:26 +0900

    Voyager 1 is 22818839716.966 kilometers away from the Sun.

---
+++ t20210416/t0002/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			if (ProcMain.DEBUG)
+			{
+				Main3();
+			}
+			else
+			{
+				Main4();
+			}
+			Common.OpenOutputDirIfCreated();
+		}
+
+		private void Main3()
+		{
+			// -- choose one --
+
+			Main4();
+			//new Test0001().Test01();
+			//new Test0002().Test01();
+			//new Test0003().Test01();
+
+			// --
+
+			//Common.Pause();
+		}
+
+		private void Main4()
+		{
+			CollectMail_File(@"C:\temp\gmail.mbox");
+		}
+
+		private void CollectMail_File(string file)
+		{
+			CollectMail(File.ReadAllLines(file, Encoding.ASCII));
+		}
+
+		private void CollectMail(string[] lines)
+		{
+			List<byte[]> messages = new List<byte[]>();
+
+			for (int index = 0; index < lines.Length; index++)
+			{
+				string line = lines[index];
+
+				if (line == "X-Mailer: stm")
+				{
+					line = lines[++index];
+
+					if (line != "")
+						throw null; // 想定外
+
+					line = lines[++index];
+
+					if (!Regex.IsMatch(line, "^[A-Za-z0-9\\+\\/]*[\\=]{0,3}$")) // ? not BASE-64
+						throw null; // 想定外
+
+					messages.Add(SCommon.Base64.I.Decode(line));
+					line = lines[++index];
+
+					if (line != "")
+						throw null; // 想定外
+				}
+			}
+
+			foreach (byte[] message in messages)
+			{
+				File.WriteAllBytes(Common.NextOutputPath() + ".txt", message);
+			}
+		}
+	}
+}
