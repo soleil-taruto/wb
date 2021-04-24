e948270ca2a62d03f195d1c28df4dbee77089428
soleil-taruto <soleil.taruto@gmail.com>
2021-04-25 08:32:37 +0900

    Voyager 1 is 22832100182.910 kilometers away from the Sun.

---
+++ t20210425_FindSync/Claes20200001/Claes20200001/Common.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using Charlotte.Commons;
+
+namespace Charlotte
+{
+	public static class Common
+	{
+		public static void Pause()
+		{
+			Console.WriteLine("Press ENTER key.");
+			Console.ReadLine();
+		}
+
+		#region GetOutputDir
+
+		private static string GOD_Dir;
+
+		public static string GetOutputDir()
+		{
+			if (GOD_Dir == null)
+				GOD_Dir = GetOutputDir_Main();
+
+			return GOD_Dir;
+		}
+
+		private static string GetOutputDir_Main()
+		{
+			for (int c = 1; c <= 999; c++)
+			{
+				string dir = "C:\\" + c;
+
+				if (
+					!Directory.Exists(dir) &&
+					!File.Exists(dir)
+					)
+				{
+					SCommon.CreateDir(dir);
+					//SCommon.Batch(new string[] { "START " + dir });
+					return dir;
+				}
+			}
+			throw new Exception("C:\\1 ～ 999 は使用できません。");
+		}
+
+		public static void OpenOutputDir()
+		{
+			SCommon.Batch(new string[] { "START " + GetOutputDir() });
+		}
+
+		public static void OpenOutputDirIfCreated()
+		{
+			if (GOD_Dir != null)
+			{
+				OpenOutputDir();
+			}
+		}
+
+		private static int NOP_Count = 0;
+
+		public static string NextOutputPath()
+		{
+			return Path.Combine(GetOutputDir(), (++NOP_Count).ToString("D4"));
+		}
+
+		#endregion
+
+		public static int GetIndentLength(string line)
+		{
+			int index;
+
+			for (index = 0; index < line.Length; index++)
+				if (line[index] != '\t')
+					break;
+
+			return index;
+		}
+
+		public static string FindProjectFileDir(string file)
+		{
+			string dir = SCommon.MakeFullPath(file);
+
+			for (; ; )
+			{
+				if (dir.Length == 3) // ? ルートDIRまで到達した。-> プロジェクトファイルが見つからない。
+					throw new Exception("プロジェクトファイルが無い。");
+
+				if (dir.Length < 3)
+					throw null; // never
+
+				dir = Path.GetDirectoryName(dir);
+
+				if (Directory.GetFiles(dir, "*.csproj").FirstOrDefault() != null) // ? プロジェクトファイルを見つけた。
+					break;
+			}
+			return dir;
+		}
+	}
+}
---
+++ t20210425_FindSync/Claes20200001/Claes20200001/SourceCodeRange.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Charlotte.Commons;
+
+namespace Charlotte
+{
+	public class SourceCodeRange
+	{
+		public readonly bool WholeFile;
+		public readonly string FilePath;
+		public readonly int LineIndexOfFile;
+		public readonly string[] Lines;
+		public readonly int IndentLength;
+		public readonly string Name;
+		public readonly string Hash;
+
+		public SourceCodeRange(string file)
+		{
+			this.WholeFile = true;
+			this.FilePath = file;
+			this.LineIndexOfFile = 0;
+			this.Lines = File.ReadAllLines(file, Encoding.UTF8);
+			this.IndentLength = 0;
+			this.Name = FileToName(file);
+			this.Hash = SCommon.Hex.ToString(SCommon.GetSHA512(File.ReadAllBytes(file)));
+		}
+
+		private static string FileToName(string file)
+		{
+			file = SCommon.MakeFullPath(file);
+			string projectFileDir = Common.FindProjectFileDir(file);
+			string name = SCommon.ChangeRoot(file, projectFileDir, "<Project>");
+			return name;
+		}
+
+		public SourceCodeRange(string file, string[] fileLines, int firstLineIndex, int lineCount, int declareLineIndex)
+		{
+			this.WholeFile = false;
+			this.FilePath = file;
+			this.LineIndexOfFile = firstLineIndex;
+			this.Lines = fileLines
+				.Skip(firstLineIndex)
+				.Take(lineCount)
+				.ToArray();
+			this.IndentLength = Common.GetIndentLength(this.Lines[0]);
+
+			if (this.IndentLength < 1)
+				throw new Exception("Bad IndentLength");
+
+			for (int index = 0; index < this.Lines.Length; index++)
+			{
+				int indentLen = Common.GetIndentLength(this.Lines[index]);
+
+				if (indentLen == 0) // ? インデント無し -- #if true など
+				{
+					// noop
+				}
+				else // ? インデント有り
+				{
+					if (indentLen < this.IndentLength) // ? 想定外のインデント幅
+						throw new Exception("Bad indentLen");
+
+					this.Lines[index] = this.Lines[index].Substring(this.IndentLength - 1); // 最小のインデント幅が1になるように調整する。
+				}
+			}
+			this.Name = fileLines[declareLineIndex].Substring(this.IndentLength);
+			this.Hash = SCommon.Hex.ToString(SCommon.GetSHA512(Encoding.UTF8.GetBytes(SCommon.LinesToText(this.Lines))));
+		}
+	}
+}
