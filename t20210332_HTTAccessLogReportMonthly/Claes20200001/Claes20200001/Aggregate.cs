933ebf9f697e0fc77d9cebb689099b2e63a5e0be
soleil-taruto <soleil.taruto@gmail.com>
2021-04-30 18:04:38 +0900

    Voyager 1 is 22839981549.299 kilometers away from the Sun.

---
+++ t20210332_HTTAccessLogReportMonthly/Claes20200001/Claes20200001/Aggregate.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using Charlotte.Commons;
+
+namespace Charlotte
+{
+	public class Aggregate : IDisposable
+	{
+		private WorkingDir WD;
+		private string CounterDir;
+		private string EntityDir;
+
+		public Aggregate()
+		{
+			this.WD = new WorkingDir();
+			this.CounterDir = this.WD.MakePath();
+			this.EntityDir = this.WD.MakePath();
+
+			SCommon.CreateDir(this.CounterDir);
+			SCommon.CreateDir(this.EntityDir);
+		}
+
+		public void Add(string line)
+		{
+			string hash = SCommon.Hex.ToString(SCommon.GetSubBytes(SCommon.GetSHA512(Encoding.UTF8.GetBytes(line)), 0, 24)); // SHA-512_192
+			string counterFile = Path.Combine(this.CounterDir, hash);
+			string entityFile = Path.Combine(this.EntityDir, hash);
+
+			if (File.Exists(counterFile))
+			{
+				long counter = long.Parse(File.ReadAllText(counterFile, Encoding.ASCII));
+				counter++;
+				File.WriteAllText(counterFile, "" + counter);
+			}
+			else
+			{
+				File.WriteAllText(counterFile, "1", Encoding.ASCII);
+				File.WriteAllText(entityFile, line, Encoding.UTF8);
+			}
+		}
+
+		public IEnumerable<string> Top(int rankMax)
+		{
+			string outFile = this.WD.MakePath();
+			string countEncHashListFile = this.WD.MakePath();
+			string orderedCountEncHashListFile = this.WD.MakePath();
+
+			SCommon.Batch(new string[] { "DIR /B > \"" + outFile + "\"" }, this.CounterDir);
+
+			using (StreamReader reader = new StreamReader(outFile, Encoding.ASCII))
+			using (StreamWriter writer = new StreamWriter(countEncHashListFile, false, Encoding.ASCII))
+			{
+				for (; ; )
+				{
+					string hash = reader.ReadLine();
+
+					if (hash == null)
+						break;
+
+					if (hash == "")
+						continue;
+
+					hash = hash.ToLower();
+
+					if (!Regex.IsMatch(hash, "^[0-9a-f]{48}$"))
+						throw new Exception("Bad hash");
+
+					string counterFile = Path.Combine(this.CounterDir, hash);
+					long count = long.Parse(File.ReadAllText(counterFile, Encoding.ASCII));
+
+					string entityFile = Path.Combine(this.EntityDir, hash);
+					string entity = File.ReadAllText(entityFile, Encoding.UTF8);
+					string miniEntity = Common.CutTrail(entity, 30);
+					string enc = SCommon.Hex.ToString(Encoding.UTF8.GetBytes(miniEntity));
+
+					writer.WriteLine(count.ToString("D19") + ":" + enc + ":" + hash);
+				}
+			}
+			SCommon.Batch(new string[] { "SORT /R \"" + countEncHashListFile + "\" /O \"" + orderedCountEncHashListFile + "\"" });
+
+			using (StreamReader reader = new StreamReader(orderedCountEncHashListFile, Encoding.ASCII))
+			{
+				long otherCount = 0;
+
+				for (int rank = 1; ; rank++)
+				{
+					string line = reader.ReadLine();
+
+					if (line == null)
+						break;
+
+					if (line == "")
+						continue;
+
+					string[] tokens = line.Split(':');
+
+					if (tokens.Length != 3)
+						throw null;
+
+					long count = long.Parse(tokens[0]);
+					//string enc = tokens[1]; // 不要
+
+					if (rank <= rankMax)
+					{
+						string hash = tokens[2];
+						string entityFile = Path.Combine(this.EntityDir, hash);
+						string entity = File.ReadAllText(entityFile, Encoding.UTF8);
+
+						yield return count + " " + entity;
+					}
+					else
+					{
+						otherCount += count;
+					}
+				}
+				if (1 <= otherCount)
+				{
+					yield return otherCount + " <その他>";
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			if (this.WD != null)
+			{
+				this.WD.Dispose();
+				this.WD = null;
+			}
+		}
+	}
+}
---
+++ t20210332_HTTAccessLogReportMonthly/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			if (ProcMain.DEBUG)
+			{
+				Main3();
+			}
+			else
+			{
+				Main4(ar);
+			}
+			//Common.OpenOutputDirIfCreated();
+		}
+
+		private void Main3()
+		{
+			// -- choose one --
+
+			new Test0001().Test01();
+			//new Test0002().Test01();
+			//new Test0003().Test01();
+
+			// --
+
+			//Common.Pause();
+		}
+
+		private int RankMax;
+		private int LogLineLenMax;
+
+		private void Main4(ArgsReader ar)
+		{
+			RankMax = int.Parse(ar.NextArg());
+			LogLineLenMax = int.Parse(ar.NextArg());
+
+			if (!Directory.Exists(Consts.LOG_DIR))
+				throw new Exception("no LOG_DIR");
+
+			string[] logFiles = P_GetFiles(Consts.LOG_DIR);
+			//string[] logFiles = Directory.GetFiles(Consts.LOG_DIR); // old
+
+			Array.Sort(logFiles, SCommon.CompIgnoreCase);
+
+			if (1 <= logFiles.Length)
+				logFiles = logFiles.Take(logFiles.Length - 1).ToArray(); // 最新のファイルは出力中かもしれないので除外する。
+
+			EraseKnownLogFiles(ref logFiles);
+			AggregateAndReport(logFiles);
+		}
+
+		public string[] P_GetFiles(string dir)
+		{
+			using (WorkingDir wd = new WorkingDir())
+			{
+				string outFile = wd.MakePath();
+				SCommon.Batch(new string[] { "DIR /B > \"" + outFile + "\"" }, dir);
+				string[] files = File.ReadAllLines(outFile, SCommon.ENCODING_SJIS);
+				files = files.Select(file => Path.Combine(dir, file)).ToArray();
+				return files;
+			}
+		}
+
+		private void EraseKnownLogFiles(ref string[] logFiles)
+		{
+			if (File.Exists(Consts.LAST_LOG_FILE_SAVE_FILE))
+			{
+				string lastLogFile = File.ReadAllText(Consts.LAST_LOG_FILE_SAVE_FILE, Encoding.UTF8);
+				int p = SCommon.IndexOf(logFiles, logFile => SCommon.EqualsIgnoreCase(logFile, lastLogFile));
+
+				if (p != -1)
+					logFiles = logFiles.Skip(p + 1).ToArray(); // pまで(pを含めて)除去する。
+			}
+			if (1 <= logFiles.Length)
+				File.WriteAllText(Consts.LAST_LOG_FILE_SAVE_FILE, logFiles[logFiles.Length - 1], Encoding.UTF8); // 更新
+		}
+
+		private void AggregateAndReport(string[] logFiles)
+		{
+			if (1 <= logFiles.Length)
+			{
+				Console.WriteLine("開始：" + logFiles[0]);
+				Console.WriteLine("終了：" + logFiles[logFiles.Length - 1]);
+			}
+
+			using (Aggregate aggrIP = new Aggregate())
+			using (Aggregate aggrRequest = new Aggregate())
+			using (Aggregate aggrHost = new Aggregate())
+			using (Aggregate aggrAgent = new Aggregate())
+			using (Aggregate aggrDomain = new Aggregate())
+			using (Aggregate aggrPath = new Aggregate())
+			using (Aggregate aggrPath_Zip = new Aggregate())
+			using (Aggregate aggrPath_IP_Zip = new Aggregate())
+			using (Aggregate aggrStatus = new Aggregate())
+			{
+				foreach (string logFile in logFiles)
+				{
+					using (StreamReader reader = new StreamReader(logFile, SCommon.ENCODING_SJIS))
+					{
+						IEnumerable<RecordInfo> records = LogLinesToRecords(ReadLogLines(reader));
+
+						records = records.Where(record => record.IP != "192.168.123.254"); // 除外：室内(内側)ルータからのリクエストのIPアドレス
+
+						records = records.Where(record => record.Host != "barnatsutobi.dip.jp");
+						records = records.Where(record => record.Host != "barnatsutobi.ccsp.mydns.jp");
+
+						foreach (RecordInfo record in records)
+						{
+							aggrIP.Add(record.IP);
+							aggrRequest.Add(record.Request);
+							aggrHost.Add(record.Host);
+							aggrAgent.Add(record.Agent);
+							aggrDomain.Add(record.Domain);
+							aggrPath.Add(record.Path);
+
+							if (SCommon.EndsWithIgnoreCase(record.Path, ".zip"))
+							{
+								aggrPath_Zip.Add(record.Path);
+								aggrPath_IP_Zip.Add(record.IP + " " + record.Path);
+							}
+							aggrStatus.Add(record.Status);
+						}
+					}
+				}
+				PrintReportLines(aggrIP, "IP");
+				PrintReportLines(aggrRequest, "Request");
+				PrintReportLines(aggrHost, "Host");
+				PrintReportLines(aggrAgent, "Agent");
+				PrintReportLines(aggrDomain, "Domain");
+				PrintReportLines(aggrPath, "Path");
+				PrintReportLines(aggrPath_Zip, "Path_Zip");
+				PrintReportLines(aggrPath_IP_Zip, "Path_IP_Zip");
+				PrintReportLines(aggrStatus, "Status");
+			}
+		}
+
+		private IEnumerable<string> ReadLogLines(StreamReader reader)
+		{
+			for (; ; )
+			{
+				string line = reader.ReadLine();
+
+				if (line == null)
+					break;
+
+				line = SCommon.ToJString(SCommon.ENCODING_SJIS.GetBytes(line), true, false, false, true); // 2bs?
+
+				if (line == "")
+					continue;
+
+				line = Common.CutTrail(line, LogLineLenMax);
+
+				yield return line;
+			}
+		}
+
+		private const string RECORD_DUMMY_VALUE = "<データ無し>";
+
+		private class RecordInfo
+		{
+			public string IP = RECORD_DUMMY_VALUE;
+			public string Request = RECORD_DUMMY_VALUE;
+			public string Host = RECORD_DUMMY_VALUE;
+			public string Agent = RECORD_DUMMY_VALUE;
+			public string Domain = RECORD_DUMMY_VALUE;
+			public string Path = RECORD_DUMMY_VALUE;
+			public string Status = RECORD_DUMMY_VALUE;
+		}
+
+		private IEnumerable<RecordInfo> LogLinesToRecords(IEnumerable<string> lines)
+		{
+			RecordInfo record = null;
+
+			foreach (string line in lines)
+			{
+				if (IsIPLine(line))
+				{
+					if (record != null)
+						yield return record;
+
+					record = new RecordInfo();
+					record.IP = line;
+				}
+				else if (line[0] == 'R')
+				{
+					record.Request = line.Substring(1);
+				}
+				else if (line[0] == 'H')
+				{
+					record.Host = line.Substring(1);
+				}
+				else if (line[0] == 'A')
+				{
+					record.Agent = line.Substring(1);
+				}
+				else if (line[0] == 'D')
+				{
+					record.Domain = line.Substring(1);
+				}
+				else if (line[0] == 'P')
+				{
+					record.Path = line.Substring(1);
+				}
+				else if (line[0] == 'S')
+				{
+					record.Status = line.Substring(1);
+				}
+			}
+			if (record != null)
+				yield return record;
+		}
+
+		private void PrintReportLines(Aggregate aggr, string title)
+		{
+			Console.WriteLine("[" + title + "]");
+
+			foreach (string line in aggr.Top(RankMax))
+			{
+				Console.WriteLine(line);
+			}
+		}
+
+		private static bool IsIPLine(string line)
+		{
+			return Regex.IsMatch(line, @"^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$");
+		}
+	}
+}
