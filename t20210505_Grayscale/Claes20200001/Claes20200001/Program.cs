da9dcba12e7083c8efd21de085035b8bac0ebb4a
soleil-taruto <soleil.taruto@gmail.com>
2021-05-06 01:15:49 +0900

    Voyager 1 is 22847720213.257 kilometers away from the Sun.

--- t20210505_Grayscale/Claes20200001/Claes20200001/Program.cs
+++ t20210505_Grayscale/Claes20200001/Claes20200001/Program.cs
@@ -48,7 +48,27 @@ namespace Charlotte
 
 		private void Main4()
 		{
-			// none
+			// -- choose one --
+
+			Conv(@"C:\Dev\Elsa2\e20210245_Hakonoko\dat\dat\ぱくたそ\kazukiphotomon04.jpg");
+
+			// --
+		}
+
+		private void Conv(string rFile)
+		{
+			Canvas canvas = Canvas.Load(rFile);
+
+			for (int x = 0; x < canvas.W; x++)
+			{
+				for (int y = 0; y < canvas.H; y++)
+				{
+					I4Color dot = canvas[x, y];
+					int level = SCommon.ToInt((dot.R + dot.G + dot.B) / 3);
+					canvas[x, y] = new I4Color(level, level, level, dot.A);
+				}
+			}
+			canvas.Save(Common.NextOutputPath() + ".png");
 		}
 	}
 }
